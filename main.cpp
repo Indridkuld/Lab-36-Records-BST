67c91bc79d13c0c9f255b38e69d3380df780bf07
Aidan <aidanmichaelwoodcock@gmail.com>
2025-11-30 21:40:42 -0800

    Milestone 2 Completed

--- main.cpp
+++ main.cpp
@@ -8,6 +8,15 @@ using namespace std;
 int main() {
     IntBinaryTree tree;
 
+    tree.insertNode("ABC123");
+    tree.insertNode("XYZ999");
+    tree.insertNode("HELLO1");
+
+    cout << "In-order traversal:\n";
+    tree.displayInOrder();
+
+    cout << "Searching for ABC123: "
+         << (tree.searchNode("ABC123") ? "found\n" : "not found\n");
 
     return 0;
 }
